1998a779ca86ef9f85c95a634368d46e1225e141
Zeeeratul <celestdelahaye@gmail.com>
2023-07-19 15:29:32 +0200

    try to deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const getConfig = require("./config");
+const galadmin = require("@galadrim/galadmin").default;
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -8,11 +9,12 @@ const PORT = process.env.PORT || 4000;
 const productionConfig = getConfig("production");
 const stagingConfig = getConfig("staging");
 
-const galadmin = require("@galadrim/galadmin").default;
-
 app.listen(PORT, () => {
   console.log(`Backoffice is running http://localhost:${PORT}`);
 });
 
 app.use("/", galadmin(productionConfig));
-// app.use("/staging", galadmin(stagingConfig));
+app.use("/staging", galadmin(stagingConfig));
+
+// Export the Express API
+module.exports = app;
