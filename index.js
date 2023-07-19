bd076e35dc90b80e03de398669eb038ea4413260
Zeeeratul <celestdelahaye@gmail.com>
2023-07-19 15:16:24 +0200

    new content

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+require("dotenv").config();
 const express = require("express");
+const getConfig = require("./config");
+
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+const PORT = process.env.PORT || 4000;
+
+const productionConfig = getConfig("production");
+const stagingConfig = getConfig("staging");
+
+const galadmin = require("@galadrim/galadmin").default;
+
+app.listen(PORT, () => {
+  console.log(`Backoffice is running http://localhost:${PORT}`);
 });
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
-});
-// Export the Express API
-module.exports = app;
+
+app.use("/", galadmin(productionConfig));
+app.use("/staging", galadmin(stagingConfig));
