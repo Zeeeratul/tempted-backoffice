2d9c2349437020973912ff36e96b762a8b96fd1a
Zeeeratul <celestdelahaye@gmail.com>
2023-07-20 14:40:28 +0200

    ui for reset password

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
-const getConfig = require("./config");
-const galadmin = require("@galadrim/galadmin");
+// const getConfig = require("./config");
+// const galadmin = require("@galadrim/galadmin");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const productionConfig = getConfig("production");
-const stagingConfig = getConfig("staging");
+// const productionConfig = getConfig("production");
+// const stagingConfig = getConfig("staging");
 
 app.listen(PORT, () => {
   console.log(`Backoffice is running http://localhost:${PORT}`);
 });
 
-console.log(galadmin);
+// console.log(galadmin);
 
 // app.use("/", galadmin(productionConfig));
 // app.use("/staging", galadmin(stagingConfig));
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.json("Hello world !");
 });
-// Export the Express API
-module.exports = app;
