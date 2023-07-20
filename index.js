a768b824461378c45f99b6da99ddd88512c0289a
Zeeeratul <celestdelahaye@gmail.com>
2023-07-20 14:49:54 +0200

    push

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
-// const getConfig = require("./config");
-// const galadmin = require("@galadrim/galadmin");
+const getConfig = require("./config");
+const galadmin = require("@galadrim/galadmin");
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// const productionConfig = getConfig("production");
-// const stagingConfig = getConfig("staging");
+const productionConfig = getConfig("production");
+const stagingConfig = getConfig("staging");
 
 app.listen(PORT, () => {
   console.log(`Backoffice is running http://localhost:${PORT}`);
 });
 
-// console.log(galadmin);
+console.log(productionConfig);
 
-// app.use("/", galadmin(productionConfig));
-// app.use("/staging", galadmin(stagingConfig));
-
-app.get("/", (req, res) => {
-  res.json("Hello world !");
-});
+app.use("/", galadmin(productionConfig));
+app.use("/staging", galadmin(stagingConfig));
